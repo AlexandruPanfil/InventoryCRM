5e0e7bff76942bf75a1f2918b03ecb766476a935
AlexandruPanfil <111560446+AlexandruPanfil@users.noreply.github.com>
2026-01-26 21:35:28 +0200

    A fost adaugata logica de mutare a unitatilor intre depozite

--- Services/DepositService.cs
+++ Services/DepositService.cs
@@ -12,10 +12,19 @@ namespace InventoryCRM.Services
             _context = context;
         }
 
-        //For Read
+        //For Read All Deposits With Units
         public async Task<List<Deposit>> GetAllDepositsAsync()
         {
             return await _context.Deposits
+                .Include(i => i.Unit)
+                .OrderBy(d => d.Name).ToListAsync();
+        }
+
+        //For Read One Deposits With Units
+        public async Task<List<Deposit>> GetOneDepositsAsync(Guid id)
+        {
+            return await _context.Deposits
+                .Where(d => d.Id == id)
                 .Include(i => i.Unit)
                 .OrderBy(d => d.Name).ToListAsync();
         }
@@ -47,7 +56,7 @@ namespace InventoryCRM.Services
             return deposit;
         }
 
-        //For Update
+        //For Update Name
         public async Task<Deposit> UpdateDepositsAsync(Guid id, string name)
         {
             var deposit = await _context.Deposits.FindAsync(id);
@@ -57,6 +66,75 @@ namespace InventoryCRM.Services
                 await _context.SaveChangesAsync();
             }
             return deposit;
+        }
+
+        //For Update Units FRom Deposit Side
+        public async Task<Deposit> UpdateDepositsUnitsAsync(Guid sourceID, Guid destinationID, ICollection<Unit> units)
+        {
+            var sourceDeposit = await _context.Deposits
+                .Include(d => d.Unit)
+                .FirstOrDefaultAsync(d => d.Id == sourceID);
+            var destinationDeposit = await _context.Deposits
+                .Include(d => d.Unit)
+                .FirstOrDefaultAsync(d => d.Id == destinationID);
+
+            if (sourceDeposit == null)
+            {
+                throw new InvalidOperationException("Source deposit not found.");
+            }
+
+            if (destinationDeposit == null) 
+            { 
+                throw new InvalidOperationException("Destination deposit not found.");
+            }
+
+            if (sourceDeposit.Unit.Count == 0)
+            {
+                throw new InvalidOperationException("Source deposit has no units to transfer.");
+            }
+
+            foreach (var unit in units)
+            {
+                var sourceUnitToTransfer = sourceDeposit.Unit.FirstOrDefault(u => u.Id == unit.Id && u.Name == unit.Name);
+                var destinationUnitToTransfer = destinationDeposit.Unit.FirstOrDefault(u => u.Name == unit.Name);
+                Unit newUnit = new Unit
+                {
+                    //Id = Guid.NewGuid(),
+                    Name = unit.Name,
+                    Quantity = unit.Quantity,
+                    DepositId = destinationDeposit.Id
+                };
+
+                if (sourceUnitToTransfer.Quantity < unit.Quantity)
+                {
+                    throw new InvalidOperationException($"Insufficient quantity in source deposit for unit {unit.Name}.");
+                }
+
+                sourceUnitToTransfer.Quantity -= unit.Quantity;
+
+                if (sourceUnitToTransfer.Quantity == 0)
+                {
+                    _context.Deposits.Where(d => d.Id == sourceDeposit.Id)
+                        .Include(d => d.Unit)
+                        .First()
+                        .Unit
+                        .Remove(sourceUnitToTransfer);
+                    _context.Units.Remove(sourceUnitToTransfer);
+                }
+
+                if (destinationUnitToTransfer != null)
+                {
+                    destinationUnitToTransfer.Quantity += unit.Quantity;
+                    continue;
+                }
+                else
+                {
+                    destinationDeposit.Unit.Add(newUnit);
+                }
+                
+            }
+            await _context.SaveChangesAsync();
+            return destinationDeposit;
         }
 
         //For Delete
